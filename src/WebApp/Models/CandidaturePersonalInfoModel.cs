2e9803b1efdf0501c4f10fe2b40c210774c66c4d
AmineTissilguit <kaminibox_2015@outlook.fr>
2022-12-06 02:59:51 +0100

    Fix frensh typing errors

--- src/WebApp/Models/CandidaturePersonalInfoModel.cs
+++ src/WebApp/Models/CandidaturePersonalInfoModel.cs
@@ -12,31 +12,30 @@ namespace WebApp.Models
         [Display(Name = "Nom")]
         public string Nom { get; set; }
 
-        [Required(ErrorMessage = "Le champ Prenom est obligatoire")]
-        [Display(Name = "Prenom")]
+        [Required(ErrorMessage = "Le champ Prénom est obligatoire")]
+        [Display(Name = "Prénom")]
         public string Prenom { get; set; }
 
         [Required(ErrorMessage = "Le champ Mail est obligatoire")]
         [EmailAddress(ErrorMessage = "Mail n'est pas valide")]
-        [Display(Name = "Mail")]
         public string Mail { get; set; }
 
-        [Required(ErrorMessage = "Le champ Telephone est obligatoire")]
-        [Display(Name = "Telephone")]
+        [Required(ErrorMessage = "Le champ Téléphone est obligatoire")]
+        [Display(Name = "Téléphone")]
         [DataType(DataType.PhoneNumber, ErrorMessage = "Telephone n'est pas valide")]
         public string Telephone { get; set; }
 
-        [Required(ErrorMessage = "Le champ NiveauEtude est obligatoire")]
-        [Display(Name = "NiveauEtude")]
+        [Required(ErrorMessage = "Le champ Niveau d'étude est obligatoire")]
+        [Display(Name = "Niveau d'étude")]
         public string NiveauEtude { get; set; }
 
-        [Required(ErrorMessage = "Le champ AnneeExperience est obligatoire")]
-        [Range(0, 20, ErrorMessage = "Le champ AnneeExperience doit être compris entre 0 et 20")]
-        [Display(Name = "Annee Experience")]
+        [Required(ErrorMessage = "Le champ Nombre d'annee d'expérience est obligatoire")]
+        [Range(0, 20, ErrorMessage = "Le champ Nombre d'annee d'expérience doit être compris entre 0 et 20")]
+        [Display(Name = "Nombre d'annee d'expérience")]
         public int AnneeExperience { get; set; }
 
-        [Required(ErrorMessage = "Le champ DernierEmployeur est obligatoire")]
-        [Display(Name = "Dernier Employeur")]
+        [Required(ErrorMessage = "Le champ Dernier employeur est obligatoire")]
+        [Display(Name = "Dernier employeur")]
         public string DernierEmployeur { get; set; }
 
     }
